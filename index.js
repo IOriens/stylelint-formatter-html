20c9974b23bfa23adaed1a51873bad75d8631fc5
Junjie Ding <djjoriens@gmail.com>
2018-03-08 16:54:06 +0800

    feat: add js

---
+++ index.js
@@ -0,0 +1,13 @@
+const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
+
+// Omit any properties starting with `_`, which are fake-private
+module.exports = function (results) {
+  const cleanedResults = results.map(result => {
+    return _.omitBy(result, (value, key) => key[0] === '_')
+  })
+
+  const tmpl = fs.readFileSync(path.resolve(__dirname, './build/index.html'))
+  return tmpl.toString().replace('LINT_RESULT', JSON.stringify(cleanedResults))
+}
--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import style from './App.css'
 import WarningList from './warning-list/'
 
 class App extends Component {
-  render () {
+  render (){
     let resultList = window.styleLintResult
     return (
       <div className={style.App}>
-        {resultList.map(res => (
+        { resultList&& resultList.length ? resultList.map(res => (
           <div className={style.resultList} key={res.source}>
             <h4>{res.source}</h4>
             <WarningList warnings={res.warnings} />
           </div>
-        ))}
+        )): <h3>恭喜您，Lint通过</h3>}
       </div>
     )
   }
